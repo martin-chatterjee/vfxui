ba53cf99e9f6fec9682e94c46eae45162d7a88cf
Martin Chatterjee <martin@chatterjee.de>
2019-07-18 11:23:50 +0200

    repo structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# vfx-ui
+# vfxui
 Rapid and easy creation of PySide based user interfaces that can be used easily inside multiple contexts (Python 2, Python 3, Maya, Nuke, Houdini, ...)
---
+++ python/vfxui/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019, Martin Chatterjee. All rights reserved.
+# Licensed under MIT License (--> LICENSE.txt)
+# -----------------------------------------------------------------------------
